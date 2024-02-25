aa6a1b07cb024e7c1473f198a97f439b7a03a2a2
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2024-02-25 10:09:04 -0500

    feat: agregate css file for color manager and comment

--- staticfiles/js/operations.js
+++ staticfiles/js/operations.js
@@ -1,3 +1,9 @@
+/* JavaScript para la funcionalidad de la calculadora */
+
+/**
+ * Agrega un valor a la pantalla de la calculadora.
+ * @param {string} value - El valor a agregar a la pantalla.
+ */
 function addToScreen(value) {
     var screen = document.getElementById('screen');
     if (screen.innerText === '0') {
@@ -7,13 +13,23 @@ function addToScreen(value) {
     }
 }
 
+/**
+ * Función para limpiar la pantalla
+ */
 function clearScreen() {
     document.getElementById('screen').innerText = '0';
 }
 
+/**
+ * Calcula el resultado de la expresión en la pantalla de la calculadora.
+ * Reemplaza el operador ^ con ** antes de evaluar la expresión.
+ * Si hay un error, muestra 'Error' en la pantalla.
+ */
 function calculate() {
     try {
-        var result = eval(document.getElementById('screen').innerText);
+        var expression = document.getElementById('screen').innerText;
+        expression = expression.replace(/\^/g, "**");
+        var result = eval(expression);
         document.getElementById('screen').innerText = result;
     } catch (error) {
         document.getElementById('screen').innerText = 'Error';
